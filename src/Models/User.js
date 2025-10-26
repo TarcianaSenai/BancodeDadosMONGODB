a272321992f27a3cebe69539a91a5571d1b465d8
Tarciana <tarciana13pereira@gmail.com>
2025-10-26 15:58:13 -0300

    Criando modelos do banco de dados

---
+++ src/Models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  birthDate: {
+    type: Date,
+    required: true,
+  },
+  sex: {
+    type: String,
+    required: true,
+  },
+  adress: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Users", userSchema);
